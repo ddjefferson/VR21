d82b99c946120f4eeed97ef15ce3c693cdd2e1ad
ddjefferson <ddjefferson@yahoo.com>
2022-11-30 08:51:27 -0800

    added alot of console logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 console.log("Is this working");
 
 const myName = "David";
+const age = 31;
 // use string literal or concatination
 
 console.log("My name: ", myName);
@@ -10,3 +11,72 @@ console.log("Type of number: ", typeof number);
 
 const isLoggedIn = true;
 console.log("Is logged in: ", isLoggedIn);
+
+favFood = "seafood";
+favRestaurant = "TruLucks";
+console.log("I love " + favRestaurant + " they have the best " + favFood + "!");
+
+console.log("6" == 6);
+
+const firstName = "David";
+const interest = "traveling";
+const hobby = "read";
+const awesomeMessage =
+  "Hi, my name is " +
+  firstName +
+  ". " +
+  "I love " +
+  interest +
+  "." +
+  " In my spare time, I like to " +
+  hobby +
+  ".";
+console.log(awesomeMessage);
+
+// sqrt Whiskey/me == true;
+
+let randomQuestion = "If i wrote you a love letter, would you write back?";
+console.log(randomQuestion);
+
+const number2 = 69;
+
+console.log(
+  "The square root of " +
+    number2 +
+    " is " +
+    Math.sqrt(69) +
+    " " +
+    " cause i've been trying to work it out"
+);
+
+console.log(3 * 12 + 10 - 4);
+
+const totalAfterTax = 53.03;
+const tip = 8;
+const total = totalAfterTax + tip;
+console.log(total);
+
+const celsius = 12;
+const fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+const answer =
+  "allegedly ALL are CrEaTeD equal" ==
+  "allegedly ALL Strings are CrEaTeD equal";
+console.log(answer);
+
+const joke =
+  'why couldn\'t the shoes go out and play?\n They were all "tied" up!';
+console.log(joke);
+
+0 == false;
+
+"1" == 1;
+
+const thingOne = "purple";
+const thingTwo = "gold";
+console.log(thingOne + " " + thingTwo);
+
+for (i = 0; i < 12; i++) {
+  console.log(i);
+}
