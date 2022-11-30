9af73afbe5de891d4dd164eeae7241a4e0c29ede
ddjefferson <ddjefferson@yahoo.com>
2022-11-30 08:11:15 -0800

    added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+console.log("Is this working");
+
+const myName = "David";
+// use string literal or concatination
+
+console.log("My name: ", myName);
+
+const number = 9;
+console.log("Type of number: ", typeof number);
+
+const isLoggedIn = true;
+console.log("Is logged in: ", isLoggedIn);
