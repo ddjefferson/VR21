8dff85657963b1bae67e92fab72a660de880577e
ddjefferson <ddjefferson@yahoo.com>
2022-12-01 15:09:57 -0800

    added objects.js and objects.html

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,7 +1,24 @@
-// let programming = {
-//   languages: ["JavaScript", "Python", "Ruby"],
-//   isChallenging: true,
-//   isRewarding: true,
-//   difficulty: 8,
-//   jokes: "https://bit.ly/2ysFran",
+// const David = {
+//   firstName: "David",
+//   lastName: "Jefferson",
+//   age: 2037 - 1991,
+//   job: programmer,
+//   friends: ["bo", "tee", "jay", "john"],
 // };
+
+let programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes: "https://bit.ly/2ysFran",
+};
+
+// Write the command to add the language “Go” to the end of the languages array.
+programming.languages.push("Go");
+// Change the difficulty to the value of 7.
+programming.difficulty = 7;
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+delete programming.jokes;
+// Write the command to add a new key called isFun and a value of true to the programming object.
+console.log(programming);
