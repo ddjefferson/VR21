5a8e631606e8e18345c133c6d7444e652ac55e0f
ddjefferson <ddjefferson@yahoo.com>
2022-12-01 12:31:48 -0800

    added functions

---
+++ assignments/functions2.js
@@ -0,0 +1,19 @@
+function logger() {
+  console.log("My name is David");
+}
+//calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+//input of the function
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
