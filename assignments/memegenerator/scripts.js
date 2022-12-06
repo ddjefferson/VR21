27e176b532ec81f6f9dc2eba91769b7c7644f7bf
ddjefferson <ddjefferson@yahoo.com>
2022-12-06 13:58:34 -0800

    updated meme gen final

--- assignments/memegenerator/scripts.js
+++ assignments/memegenerator/scripts.js
@@ -1,21 +1,21 @@
 document.querySelector("#submit").addEventListener("click", (e) => {
   e.preventDefault();
   const image = document.querySelector("#image").value;
-  const topQuote = document.querySelector("#top-quote").value;
-  const bottomQuote = document.querySelector("#bottom-quote").value;
+  const topText = document.querySelector("#top-quote").value;
+  const bottomText = document.querySelector("#bottom-quote").value;
 
   const contentCard = `
   <div class="col-md-3">
          <div class="card">
          <div class="card-body">
-               <h3 class="top-quote">${topQuote}</h3>
+               <h3 class="top-quote">${topText}</h3>
            </div>
            <img
              src=${image}
              class="card-img-top"
              alt="..."/>
            <div>
-              <h3 class="bottom-quote">${bottomQuote}</h3>
+              <h3 class="bottom-quote">${bottomText}</h3>
            </div>
          </div>
        </div>
