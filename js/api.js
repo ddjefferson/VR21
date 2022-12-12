8bb9cd0320e14e618ea59339f9a3165cee37388a
ddjefferson <ddjefferson@yahoo.com>
2022-12-12 15:10:05 -0800

    added color picker

---
+++ js/api.js
@@ -0,0 +1,21 @@
+// get api key
+const clientID = "z6ilhTY9AvnYQCJnM8qDSae90VMSH82ed4JZOgv7DiE";
+// capture search term
+const searchTerm = "aircrafts";
+
+const resource = " "
+
+// make a request to unsplash api
+fetch(resource)
+.then((res) => res.json());
+.then((data) => {
+  debugger;
+  console.log(data)
+})
+
+// capture the api data
+
+// loop over the data
+
+
+// append the dom
---
+++ js/list.js
@@ -0,0 +1,6 @@
+"use strict";
+name = "David";
+age = 31;
+
+console.log("Name: ", name);
+console.log("Age:", age);
