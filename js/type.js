fc2816e24e029ba88817eb03ee3733e54fc8e093
ddjefferson <ddjefferson@yahoo.com>
2022-12-13 13:04:46 -0800

    updated file structure

--- js/type.js
+++ js/type.js
@@ -19,20 +19,21 @@ let names = [
   "Stephanie",
   "Uyen",
   "Vlad",
+  "Nick",
 ];
-
-// sort names in ascending order
+//Sort names in ascending order
 let sortedNames = names.sort();
-console.log(sortedNames);
 
-// reference
+//reference
 let input = document.getElementById("input");
 
-// Excecute function on keyup
+//Execute function on keyup
 input.addEventListener("keyup", (e) => {
   //loop through above array
+  //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+  removeElements();
   for (let i of sortedNames) {
-    // convert input to lowerCase and compare with each string
+    //convert input to lowercase and compare with each string
 
     if (
       i.toLowerCase().startsWith(input.value.toLowerCase()) &&
@@ -40,14 +41,14 @@ input.addEventListener("keyup", (e) => {
     ) {
       //create li element
       let listItem = document.createElement("li");
-      // One common class name
+      //One common class name
       listItem.classList.add("list-items");
       listItem.style.cursor = "pointer";
-      listItem.setAttribute("onclick", "displayNames(' " + i + " ')");
-      // display matched part in bold
-      let word = "<b>" + i.substring(0, input.value.length) + "</b>";
-      word += i.substring(input.value.length);
-      // display the value in array
+      listItem.setAttribute("onclick", "displayNames('" + i + "')");
+      //Display matched part in bold
+      let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+      word += i.substr(input.value.length);
+      //display the value in array
       listItem.innerHTML = word;
       document.querySelector(".list").appendChild(listItem);
     }
@@ -55,4 +56,12 @@ input.addEventListener("keyup", (e) => {
 });
 function displayNames(value) {
   input.value = value;
+  removeElements();
+}
+function removeElements() {
+  //clear all the item
+  let items = document.querySelectorAll(".list-items");
+  items.forEach((item) => {
+    item.remove();
+  });
 }
