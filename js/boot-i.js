45dd456e849056edf2fb739710cb628f69ab537d
ddjefferson <ddjefferson@yahoo.com>
2022-12-14 07:20:21 -0800

    updated boot i

---
+++ js/boot-i.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const myVar = "some data";
+
+console.log("My data: ", myVar);
--- js/list.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-name = "David";
-age = 31;
-
-console.log("Name: ", name);
-console.log("Age:", age);
